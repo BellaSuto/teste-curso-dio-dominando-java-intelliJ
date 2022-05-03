6e58158861f86bae3b1a9a8ee69ac7cc88385ae6
Bella <bellasuto2@gmail.com>
2022-05-03 15:12:11 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgam.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgam {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("Nina", "amarelo", 07 );
+
+        System.out.println(gato);
+    }
+}
+
