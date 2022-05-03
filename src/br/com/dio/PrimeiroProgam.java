7570523e6e9dfef8fd8ff62c52e2a4b06ab07f07
Bella <bellasuto2@gmail.com>
2022-05-03 15:22:45 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroProgam.java
+++ src/br/com/dio/PrimeiroProgam.java
@@ -8,6 +8,44 @@ public class PrimeiroProgam {
         Gato gato = new Gato("Nina", "amarelo", 07 );
 
         System.out.println(gato);
+
+        Livro livro = new Livro("Rainha Vermelha",455 );
+        System.out.println(livro);
+    }
+
+}
+
+class Livro{
+    private String nome;
+    private int numPaginas;
+
+    public Livro(String nome, int numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
 
